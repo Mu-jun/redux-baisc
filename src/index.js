8f9991608477d06f5f238ccab91f5ca0d552a7bd
Mu-jun <suo24691@gmail.com>
2023-10-09 19:34:27 +0900

    redux store and reducer

--- src/index.js
+++ src/index.js
@@ -1,22 +1,12 @@
+import { legacy_createStore as createStore } from 'redux';
+
 const add = document.getElementById('add');
 const minus = document.getElementById('minus');
 const number = document.querySelector('span');
 
-let count = 0;
-
-number.innerText = count;
-
-const updateText = () => {
-  number.innerText = count;
+const countModifier = (state = 0) => {
+  return state;
 };
-const handleAdd = () => {
-  count += 1;
-  updateText();
-};
-const handleMinus = () => {
-  count -= 1;
-  updateText();
-};
+const countStore = createStore(countModifier);
 
-add.addEventListener('click', handleAdd);
-minus.addEventListener('click', handleMinus);
+console.log(countStore.getState());
